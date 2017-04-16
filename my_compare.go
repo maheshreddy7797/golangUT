1a2357143893ee29c52c00718c41598591fc9ee1
Mahesh Reddy <maheshreddy7797@gmail.com>
2017-04-16 15:20:08 +0530

    Update my_compare.go

--- my_compare.go
+++ my_compare.go
@@ -1,4 +1,4 @@
-package main
+package hacker
 // Compare returns an integer comparing two strings lexicographically.
 func Compare(a, b string) int {
 // The result will be 0 if a==b, -1 if a < b, and +1 if a > b.
